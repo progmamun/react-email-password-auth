d79e8be422c16294ccd42f60f839fe3f7cb3c077
progmamun <mamun333089@gmail.com>
2022-04-07 14:49:11 +0600

    form-input-blur

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import './App.css';
 import app from './firebase.init';
 
 const auth = getAuth(app);
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+};
+const handlePasswordBlur = event => {
+  console.log(event.target.value);
+};
+const handleFormSubmit = event => {
+  console.log('form submitted');
+  event.preventDefault();
+};
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form action="" onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
